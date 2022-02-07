1e5234d5404d39b0f8b6d3d87b247e6f69f2ee92
BEDongryeol <ehdfuf53@gmail.com>
2022-02-07 19:57:32 +0900

    #18 feat: UserTransactionHistory service layer 생성

---
+++ src/main/java/com/moa/finance/service/UserTransactionHistoryService.java
@@ -0,0 +1,33 @@
+package com.moa.finance.service;
+
+import com.moa.finance.repository.dummy.BankTransactionHistoryRepository;
+import com.moa.finance.vo.finance.UserTransactionHistory;
+import com.moa.util.mapper.TransactionHistoryMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserTransactionHistoryService {
+
+    private final BankTransactionHistoryRepository bankTransactionHistoryRepository;
+
+    @Async("preLoading")
+    public CompletableFuture run(Long userId) {
+        return new AsyncResult(getTransactionHistory(userId)).completable();
+    }
+
+    @Cacheable(value = "transactionHistoryCache")
+    public List<UserTransactionHistory> getTransactionHistory(Long userId){
+        log.info("Async Running");
+        return TransactionHistoryMapper.INSTANCE.toUserList(bankTransactionHistoryRepository.getHistoriesByAccountId(userId));
+    }
+}
