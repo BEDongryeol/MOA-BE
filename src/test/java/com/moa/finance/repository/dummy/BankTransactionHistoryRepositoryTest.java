ae9bbe70ac312096e958d79f5d5893493aa99ac2
BEDongryeol <ehdfuf53@gmail.com>
2022-02-07 19:51:41 +0900

    #18 test: BankTransactionHistory test
    1. 특정계좌의 거래내역 가져오기

---
+++ src/test/java/com/moa/finance/repository/dummy/BankTransactionHistoryRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.moa.finance.repository.dummy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionHistoryRepositoryTest {
+    @Autowired
+    private BankTransactionHistoryRepository bankTransactionHistoryRepository;
+
+    @DisplayName("1. 특정 계좌의 거래내력 조회")
+    @Test
+    void getHistoriesTest(){
+        bankTransactionHistoryRepository.getHistoriesByAccountId(4L).forEach(System.out::println);
+    }
+
+
+
+}
