4347f8ec1dbe49fd11e02b7d5c9cab8a8da4fe90
BEDongryeol <ehdfuf53@gmail.com>
2022-02-07 19:55:22 +0900

    #2 feat: UserAccount Repo 생성

---
+++ src/main/java/com/moa/finance/repository/finance/UserAccountRepository.java
@@ -0,0 +1,18 @@
+package com.moa.finance.repository.finance;
+
+import com.moa.finance.vo.finance.UserAccount;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    // User가 연동한 계좌 데이터 조회
+    @Query(value = "select u from UserAccount u " +
+                   "where u.user.id = :userId")
+    List<UserAccount> findUserAccounts(@Param("userId") Long userId);
+
+}
