f0588b03b34299a3ac65cef778d1f60be9fe8162
BEDongryeol <ehdfuf53@gmail.com>
2022-02-07 19:48:42 +0900

    #2 feat: BankSavingProducts Entity 필드 수정

--- src/main/java/com/moa/finance/vo/dummy/BankSavingProducts.java
+++ src/main/java/com/moa/finance/vo/dummy/BankSavingProducts.java
@@ -10,12 +10,13 @@ import java.math.BigDecimal;
 @Entity
 @Getter
 @ToString
-public class SavingProducts {
-    // id, 은행, 상품명, 최고 금리, 최고 금리 기준, 기본 금리, 기본 금리 기준, 금액 설명, 기간
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class BankSavingProducts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String bankImageUrl;
-    private String bankName;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "BANK_ID")
+    private Bank bank;
     private String productName;
     private BigDecimal highestInterest;
     private BigDecimal basicInterest;
