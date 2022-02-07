a8c9b83b30e4f848c74648afc765c399159b37a1
BEDongryeol <ehdfuf53@gmail.com>
2022-02-07 20:31:16 +0900

    inwoo pr

--- src/main/java/com/moa/user/vo/User.java
+++ src/main/java/com/moa/user/vo/User.java
@@ -1,7 +1,27 @@
-package com.moa.user;
+package com.moa.user.vo;
+
+import com.moa.finance.vo.finance.UserAccount;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@ToString(exclude = "userAccount")
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserAccount> userAccount = new ArrayList<>();
 }
