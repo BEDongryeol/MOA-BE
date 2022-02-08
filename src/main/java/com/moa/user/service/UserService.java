8893acf289c7376214a9def7ed52f544b42ed9ce
uunaap <whdbsk97@gmail.com>
2022-02-08 13:29:28 +0900

    #25 feat : 유저 service Layer 생성

---
+++ src/main/java/com/moa/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.moa.user.service;
+
+import com.moa.user.repository.UserRepository;
+import com.moa.user.vo.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+}
