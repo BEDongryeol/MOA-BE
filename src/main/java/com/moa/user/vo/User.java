46baf43dc295f09cdd7a715cd7366fb1897a1e22
BEDongryeol <ehdfuf53@gmail.com>
2022-02-08 13:31:12 +0900

    conf: Apply .gitignore

--- src/main/java/com/moa/user/vo/User.java
+++ src/main/java/com/moa/user/vo/User.java
@@ -4,6 +4,7 @@ import com.moa.challenge.mychallenge.vo.MyChallenge;
 import com.moa.finance.vo.finance.UserAccount;
 import lombok.*;
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,23 +23,23 @@ public class User {
 
     private String name;                    //
 
-    private String birthDate;               //
+    private LocalDate birthDate;               //
 
     private String phoneNum;                //
 
     @OneToMany(mappedBy = "user")           //주인은 account가 되고, ,account에서 JoinColumn을 해준다.
     private List<UserAccount> userAccount = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_SERVICE_INFO_ID")    //군인정보와 1:1연결
-    private UserServiceInfo userService;        //UserService class생성하고 entity 추가하기, 그리고 maaped by
+    private UserServiceInfo userServiceInfo;        //UserService class생성하고 entity 추가하기, 그리고 maaped by
 
     private Integer key;                    //
 
 //    @OneToMany(mappedBy = "user")           //주인은 userGoal가 되고, ,userGoal에서 JoinColumn을 해준다.
 //    private List<UserGoal> userGoal = new ArrayList<>();
 
-    @OneToMany                              //주인은 myChallange가 되고, ,myChallange에서 JoinColumn을 해준다.
+    @OneToMany                             //주인은 myChallange가 되고, ,myChallange에서 JoinColumn을 해준다.
     @JoinColumn(name = "mychallange_id")
     private List<MyChallenge> mychallenge = new ArrayList<>();
 
