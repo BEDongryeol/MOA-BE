5d49e361bf9d7b88e7857997f87c2d0f5291d471
BEDongryeol <ehdfuf53@gmail.com>
2022-02-08 13:27:12 +0900

    #12 test: UserRepositoryTest test 수정
    - 자료형 변경

--- src/test/java/com/moa/user/repository/UserRepositoryTest.java
+++ src/test/java/com/moa/user/repository/UserRepositoryTest.java
@@ -1,16 +1,11 @@
 package com.moa.user.repository;
 
-import com.moa.finance.repository.AccountRepository;
-import com.moa.finance.vo.dummy.TransactionHistory;
-import com.moa.finance.vo.finance.Account;
 import com.moa.user.vo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.LocalDate;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -25,7 +20,7 @@ class UserRepositoryTest {
         user.setLoginId("");
         user.setPw("");
         user.setPhoneNum("010-1111-2222");
-        user.setBirthDate("1997-09-03");
+        user.setBirthDate(LocalDate.of(1997,9,3));
 
         User newUser = userRepository.save(user);
         System.out.println(newUser);
