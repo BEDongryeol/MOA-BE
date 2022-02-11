2dd4be0909a48dcebdd14c9085de57edf3303931
uunaap <whdbsk97@gmail.com>
2022-02-11 22:51:05 +0900

    #12 feat : 유저 entity 갱신

--- src/main/java/com/moa/user/vo/User.java
+++ src/main/java/com/moa/user/vo/User.java
@@ -22,6 +22,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)    //mysql로 바꾸면 identity로
     private Long id;                        //
 
+    @Column(unique = true)
     private String loginId;                 //
 
     private String pw;                      //
@@ -59,6 +60,7 @@ public class User implements UserDetails {
     private List<RegistrationManagement> registrationManagement = new ArrayList<>();
 
 
+    //UserDetail 관련
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Set<GrantedAuthority> roles = new HashSet<>();
@@ -68,40 +70,40 @@ public class User implements UserDetails {
         return roles;
     }
 
-    // 사용자의 id를 반환 (unique한 값)
+    /// 사용자의 id를 반환 (unique한 값)
     @Override
     public String getUsername() {
         return loginId;
     }
 
-    // 사용자의 password를 반환
+    /// 사용자의 password를 반환
     @Override
     public String getPassword() {
         return pw;
     }
 
-    // 계정 만료 여부 반환
+    /// 계정 만료 여부 반환
     @Override
     public boolean isAccountNonExpired() {
         // 만료되었는지 확인하는 로직
         return true; // true -> 만료되지 않았음
     }
 
-    // 계정 잠금 여부 반환
+    /// 계정 잠금 여부 반환
     @Override
     public boolean isAccountNonLocked() {
         // 계정 잠금되었는지 확인하는 로직
         return true; // true -> 잠금되지 않았음
     }
 
-    // 패스워드의 만료 여부 반환
+    /// 패스워드의 만료 여부 반환
     @Override
     public boolean isCredentialsNonExpired() {
         // 패스워드가 만료되었는지 확인하는 로직
         return true; // true -> 만료되지 않았음
     }
 
-    // 계정 사용 가능 여부 반환
+    /// 계정 사용 가능 여부 반환
     @Override
     public boolean isEnabled() {
         // 계정이 사용 가능한지 확인하는 로직
