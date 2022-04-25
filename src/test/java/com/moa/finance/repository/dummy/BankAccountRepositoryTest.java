9880085b7fe6bb7324c107f54102e16ce6c0d598
BEDongryeol <ehdfuf53@gmail.com>
2022-04-25 12:53:48 +0900

    test: Querydsl 검색 메서드 테스트

--- src/test/java/com/moa/finance/repository/dummy/BankAccountRepositoryTest.java
+++ src/test/java/com/moa/finance/repository/dummy/BankAccountRepositoryTest.java
@@ -1,21 +1,26 @@
 package com.moa.finance.repository.dummy;
 
+import com.moa.finance.dto.searchCondition.AccountSearchCondition;
 import com.moa.finance.vo.dummy.BankTransactionHistory;
 import com.moa.finance.vo.finance.Account;
 import com.moa.finance.vo.dummy.BankAccount;
 import com.moa.constant.AccountType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
+@Transactional
 class BankAccountRepositoryTest {
 
     @Autowired
@@ -23,7 +28,6 @@ class BankAccountRepositoryTest {
 
     @DisplayName("1. 유저의 계좌 불러오기")
     @Test
-    @Transactional
     void getUsersAccount(){
         BankAccount account = bankAccountRepository.findById(1L).orElse(null);
         System.out.println(account);
@@ -38,7 +42,6 @@ class BankAccountRepositoryTest {
 
     @DisplayName("3. Unique Constraints 테스트")
     @Test
-    @Transactional
     void uniqueConstraintsTest(){
         Account account = new Account();
         account.setOwner("정인우");
@@ -58,6 +61,54 @@ class BankAccountRepositoryTest {
         BankAccount bankAccount2 = new BankAccount();
         bankAccount2.setAccount(account2);
 
+    }
+
+    @Test
+    @Commit
+    public void searchAccountTest_querydsl() throws Exception {
+        // given
+
+        Account account = new Account();
+        account.setOwner("정인우");
+        account.setBirthDate(LocalDate.now());
+        account.setAccountNumber("111-222");
+        account.setAccountType(AccountType.입출금);
+        account.setProductName("장병내일준비적금");
+
+        Account account2 = new Account();
+        account2.setOwner("정인우");
+        account2.setBirthDate(LocalDate.now().plusDays(1L));
+        account2.setAccountNumber("112-222");
+        account2.setAccountType(AccountType.입출금);
+        account2.setProductName("장병내일준비적금");
+
+        BankAccount bankAccount1 = new BankAccount();
+        bankAccount1.setAccount(account);
+
+        BankAccount bankAccount2 = new BankAccount();
+        bankAccount2.setAccount(account2);
+
+        bankAccountRepository.save(bankAccount1);
+        bankAccountRepository.save(bankAccount2);
+
+        AccountSearchCondition condition = new AccountSearchCondition();
+        condition.setBirthDate(LocalDate.now());
+        condition.setOwner("정인우");
+        condition.setProductName1("장병");
+        condition.setProductName2("나라사랑");
+
+        // when
+        List<BankAccount> result = bankAccountRepository.getAccountsByQuerydsl(condition);
+
+        // then
+        for (BankAccount bankAccount : result) {
+            System.out.println(bankAccount);
+        }
+
+        assertAll(
+                () -> assertEquals(result.size(), 1),
+                () -> assertThat(result).extracting("Account.accountNumber").containsExactly("111-222")
+        );
 
     }
 }
